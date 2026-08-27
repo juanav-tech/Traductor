34b55ccde6b36a542a42b29805d5cad621bca7f3
juanav-tech <juanavasur@gmail.com>
2026-08-27 07:15:46 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -19,7 +19,7 @@ st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
 
-image = Image.open('translate.jpg')
+image = Image.open('translate.JPG')
 
 st.image(image,width=300)
 with st.sidebar:
